75d9b35df6500cc3fc7d297e8c165dc82525592f
EuneeChung <chungee375@gmail.com>
2019-11-02 22:19:12 +0900

    Activity 생성

--- app/src/main/java/cjdgk7/s/a25thsopkathon/AnswerActivity.kt
+++ app/src/main/java/cjdgk7/s/a25thsopkathon/AnswerActivity.kt
@@ -3,11 +3,11 @@ package cjdgk7.s.a25thsopkathon
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
-class MainActivity : AppCompatActivity() {
+class AnswerActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
-
+        setContentView(R.layout.activity_answer)
+        //
     }
 }
--- app/src/main/java/cjdgk7/s/a25thsopkathon/QusetionActivity.kt
+++ app/src/main/java/cjdgk7/s/a25thsopkathon/QusetionActivity.kt
@@ -3,11 +3,11 @@ package cjdgk7.s.a25thsopkathon
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
-class MainActivity : AppCompatActivity() {
+class QusetionActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
-
+        setContentView(R.layout.activity_qusetion)
+        //
     }
 }
